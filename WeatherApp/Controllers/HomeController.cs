1922da91538397a1411ff89d2a52f3f3bd129a55
LovelessCodes <hello@loveless.codes>
2022-09-21 12:14:11 +0200

    Add project files.

---
+++ WeatherApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Json;
+using WeatherApp.Models;
+
+namespace WeatherApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var forecast = httpClient.GetAsync(Request.GetDisplayUrl() + "api/WeatherAPIConductor");
+            string fucku = await forecast.Result.Content.ReadAsStringAsync();
+            var json = JsonSerializer.Deserialize<Root>(fucku);
+            return View(json);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WeatherApp/Controllers/WeatherAPIConductor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Models;
+
+
+namespace WeatherApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherAPIConductor : ControllerBase
+    {
+        [HttpGet]
+        public async Task<Root> Index() {
+            var httpClient = new HttpClient();
+            var forecast = httpClient.GetAsync("http://api.openweathermap.org/data/2.5/forecast?id=2615876&appid=af5bcb9ea1e67227ac9c7ae49d00eab1");
+            var json = await forecast.Result.Content.ReadFromJsonAsync<Root>();
+            return json;
+        }
+        [HttpGet("{id}")]
+        public async Task<Root> Index(int id)
+        {
+            var httpClient = new HttpClient();
+            var forecast = httpClient.GetAsync("http://api.openweathermap.org/data/2.5/forecast?id=" + id + "&appid=af5bcb9ea1e67227ac9c7ae49d00eab1");
+            var json = await forecast.Result.Content.ReadFromJsonAsync<Root>();
+            return json;
+        }
+    }
+}
